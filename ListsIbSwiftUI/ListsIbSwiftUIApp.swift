b207251e4f7b8a93e20ab777077e9c5c12bbc248
rebazcg <ostan.jaff@gmail.com>
2024-07-18 22:33:56 +0300

    Initial Commit

---
+++ ListsIbSwiftUI/ListsIbSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsIbSwiftUIApp.swift
+//  ListsIbSwiftUI
+//
+//  Created by Rebaz Salahaddin on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ListsIbSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
